05ebf7c7d13b043b46477e79ece1ae04da44f598
Whitney Reans <wreans@yahoo.com>
2015-02-27 13:57:09 -0800

    add initial files

---
+++ README.md
@@ -0,0 +1,11 @@
+##Newspaper
+
+Newspaper is a site that allows a newspaper owner to enter "beats", or categories of the paper, and add reporters who are assigned to these beats, along with their birthdates and years of employment.
+
+##Credits
+
+Whitney Reans
+
+##License
+
+MIT License
---
+++ controllers/ReportersController.js
@@ -0,0 +1,10 @@
+newsPaper.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams, BeatsFactory, UtilitiesFactory) {
+  $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId)
+  $scope.addReporter = function() {
+    $scope.beat.reporters.push({ name: $scope.reporterName, yearsOfEmployment: $scope.reporterYears, birthday: $scope.reporterBirthday });
+    $scope.reporterName = null;
+    $scope.reporterYears = null;
+    $scope.reporterBirthday = null;
+
+  }
+});
